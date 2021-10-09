824a5a6f180430a9decbc92ac8c10c2b34ffe642
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-10-09 23:07:04 +0530

    Add files via upload

---
+++ to find the perimeter of rectangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{float length,breadth,perimeter;
+printf("enter the length of rectangle:");
+scanf("%f",&length);
+printf("enter the breadth of rectangle:");
+scanf("%f",&breadth);
+perimeter=2*(length+breadth);
+printf(" perimeter of rectangle=%f",perimeter);
+
+
+
+return 0;
+}
