51d8fd233de32a88e211cf192918a5f3f0273b45
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-10-09 13:43:33 +0530

    Add files via upload

---
+++ to find the area of trapezium.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    float b1,b2,h;
+    float area;
+
+    printf("Enter the size of two bases and height of the trapezium : ");
+    scanf("%f%f%f",&b1,&b2,&h);
+
+    area = 0.5 * ( b1 + b2 ) * h ;
+
+    printf("Area of trapezium is: %.3f",area);
+
+    return 0;
+}
