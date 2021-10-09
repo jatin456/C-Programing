5304f5e6afcb441ae39670e8f5d5c58854c4b318
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-10-09 23:15:26 +0530

    Add files via upload

---
+++ to find the area of rectangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{float length,breadth,area;
+printf("enter the length of rectangle:");
+scanf("%f",&length);
+printf("enter the breadth of rectangle:");
+scanf("%f",&breadth);
+area=(length*breadth);
+printf(" area of rectangle=%f",area);
+
+
+
+return 0;
+}
