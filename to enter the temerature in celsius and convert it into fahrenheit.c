2d24e68c3553f842d06e1bf37d5bb697442e82ec
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-10-10 11:26:09 +0530

    Add files via upload

---
+++ to enter the temerature in celsius and convert it into fahrenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{float celsius,fahrenheit;
+printf("enter the temperatue in celsius:");
+scanf("%f",&celsius);
+fahrenheit=((9*celsius)/5+32);
+printf("%f",fahrenheit);
+return 0;
+}
