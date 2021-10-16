ae5ee1146265b145685e66f6fad3c8edc11d6689
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-10-16 22:40:22 +0530

    Add files via upload

---
+++ the find the area of triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float base,height,area;
+printf("Enter the base of triangle:");
+scanf("%f",&base);
+printf("Enter the height of triangle:");
+scanf("%f",&height);
+area=((base*height)/2);
+printf("The area of triangle is %f",area);
+	return 0;
+	
+	
+}
