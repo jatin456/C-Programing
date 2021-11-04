03b36660b48c59f4c24b9f185bee1ef3bdc6356f
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-11-04 22:48:20 +0530

    Add files via upload

---
+++ to find the maximum numbers between two numbers inputed by the user.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{int x,y;
+printf("Enter the value of x:");
+scanf("%d",&x);
+printf("Enter the value of y:");
+scanf("%d",&y);
+if(x>y)
+{printf("x is greater");
+}
+else 
+printf("y is greater");
+
+return 0;
+	
+
+}
