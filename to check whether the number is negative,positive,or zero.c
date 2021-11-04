8e0df7b52741936c77c476cf979cd825bb18cdc5
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-11-04 23:38:41 +0530

    Add files via upload

---
+++ to check whether the number is negative,positive,or zero.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{int x;
+printf("Enter the value of x:");
+scanf("%d",&x);
+if(x<0)
+{printf("x is negative");
+}
+else if(x>0)
+{printf("x is positive");
+}
+else if(x==0)
+{printf("x is zero");}
+return 0;
+}
+    
+    
+    
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+
+	
+
