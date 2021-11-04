ac5c98b65bf845f491bbfddf288065baafaadf9a
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-11-04 23:03:09 +0530

    Add files via upload

---
+++ to find the maximum number between three numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("Enter the value of x:");
+scanf("%d",&x);
+printf("Enter the value of y:");
+scanf("%d",&y);
+printf("Enter the value of z:");
+scanf("%d",&z);
+if(x>y&&y>z)
+{printf("x is greater");
+}
+else if(y>x&&y>z)
+{printf("y is greater");
+}
+else if ("z>y&&z>x")
+{printf("z is greater");
+}
+	return 0;
+	
+}
