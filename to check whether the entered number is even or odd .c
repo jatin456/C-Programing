3b544534bb5bfcc29dca8d04ace66134b8aee31b
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-11-04 23:49:56 +0530

    Add files via upload

---
+++ to check whether the entered number is even or odd .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{int x;
+printf("Enter the value of x:");
+scanf("%d",&x);
+if(x%2==0)
+{printf("x is even");
+}
+else{printf("x is odd");
+}
+
+
+
+	
+
+
+
+
+return 0;
+}
