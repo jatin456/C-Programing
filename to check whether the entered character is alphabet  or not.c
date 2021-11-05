40fe07833b989cab5530d83f6366e52ded3ed459
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-11-05 13:37:19 +0530

    Add files via upload

---
+++ to check whether the entered character is alphabet  or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{char ch;
+printf("Enter the character:");
+scanf("%c",&ch);
+if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+{printf("It is alphabet");
+}
+	else{printf("It is not alphabet");
+	}
+	
+	return 0;
+	
+}
