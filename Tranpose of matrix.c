aea0705785524ac5dffdb4970ab29fe3b05562e3
jatin456 <92090647+jatin456@users.noreply.github.com>
2021-12-21 23:52:34 +0530

    Add files via upload

---
+++ Tranpose of matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int row,col,sum;
+	int x[3][3];
+	printf("Enter the elements for matrix:\n");
+	for(row=0;row<3;row++)
+{   for(col=0;col<3;col++)
+     {
+				 printf("Elements in [%d],[%d]=",row,col);
+				 scanf("%d",&x[row][col]);
+	} 
+	}
+	     printf("\n");
+		
+	printf("The matrix is:\n");
+	printf("\n\n");
+	for(row=0;row<3;row++)
+	{
+		for(col=0;col<3;col++)
+	{   
+	      printf("%d\t",x[col][row]);
+			
+			}
+	
+				printf("\n");
+				}
+		return 0;
+}
