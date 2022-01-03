d8ae7d5ea78136e1a63c0b10fe247bf9c09921fb
jatin456 <92090647+jatin456@users.noreply.github.com>
2022-01-03 20:02:15 +0530

    Add files via upload

---
+++ WAP to input a matrix and print its upper triangular matrix.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int m,n;
+	printf("Enter the number of row:");
+	scanf("%d",&m);
+	printf("Enter the number of column:");
+	scanf("%d",&n);
+	int x[m][n],i,j;
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&x[i][j]);
+		}
+	}
+	printf("The matrix you inputed:\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",x[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("--------------------\n");
+	printf("The upper half matrix:\n");
+	for(i=0;i<m;i++)
+	{
+		printf("\n");
+		for(j=0;j<n;j++)
+		{
+			if(i<=j)
+			printf("%d\t",x[i][j]);
+		}
+		printf("\n");
+	}
+		return 0;
+}
