4b658a775f2f5d49be8abc32ddf14b0a8af28e55
jatin456 <92090647+jatin456@users.noreply.github.com>
2022-01-07 19:19:49 +0530

    Add files via upload

---
+++ sum by argument with no return.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+	void sum(int a,int b)
+	{
+		int z;
+		z=a+b;
+		printf("Sum=%d",z);
+	}
+	int main()
+	{
+		int x,y;
+		scanf("%d %d",&x,&y);
+		sum(x,y);
+		return 0;
+	}
+	
+	
+	
+	
+	
+
